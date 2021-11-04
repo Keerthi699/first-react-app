cc58fd0c208b11c2a4f092f72988b662da76413d
Mustafa Can Palaz <mustafacanpalaz@gmail.com>
2021-11-04 10:42:51 +0300

    First git commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    // 2. set user data to `users` state with `setUsers` function
+    setUsers(response.data);
+  };
+
+  // 1. get users data when user load the page
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  // watch `users` state and log if it changes
+  useEffect(() => {
+    if (users.length > 0) {
+      console.log(users);
+    }
+  }, [users]);
+
+  // 3. show users from the `users` state
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((user) => (
+        <div key={user.id}>
+          <li style={{ listStyle: "none" }}>{user.name}</li>
+          <li style={{ listStyle: "none", color: "#888", marginBottom: 20 }}>
+            {user.email}
+          </li>
+        </div>
+      ))}
     </div>
   );
 }
